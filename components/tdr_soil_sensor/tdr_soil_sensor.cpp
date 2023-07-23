16defbd56480babee7958e696e4cb16c62832b14
infotronic218 <infotronic218@gmail.com>
2023-07-23 12:08:46 +0200

    update the source code

--- components/tdr_soil_sensor/tdr_soil_sensor.cpp
+++ components/tdr_soil_sensor/tdr_soil_sensor.cpp
@@ -1,18 +1,50 @@
 #include"tdr_soil_sensor.h"
 #include "esphome/core/log.h"
+#include <SoftwareSerial.h>
+#include <Wire.h>
+
+
+static const float TEMP_CORRECTION = 0.5;
+static const float HUM_CORRECTION = 0.8;
+static const float EC_SLOPE = 1.93;
+static const float EC_INTERCEPT = -270.8;
+static const float EC_TEMP_COEFF = 0.019;
+static const char *TAG = "TDR Soil Sensor" ;
+static EspSoftwareSerial::UART mod;
+
 
 namespace esphome {
   namespace  tdr_soil_sensor_{
-     void TDR_Soil_Sensor::setup(void){
+     void TDR_Soil_Sensor::setup(){
+      ESP_LOGI(TAG, "Initialization");
+       pinMode(pin_re, OUTPUT);
+       pinMode(pin_de, OUTPUT);
+       digitalWrite(pin_re, LOW);
+       digitalWrite(pin_de, LOW);
+       //SoftwareSerial mod(D6, D5);
 
-     }
+       mod.begin(38400, SWSERIAL_8N1, pin_rx, pin_tx, false);
+       if(!mod){
+        ESP_LOGE(TAG, "Error Initializing the Modbus Serial");
+        ESP_LOGE(TAG, "Please check  the provided RX, TX pins and try again");
+       }else {
+        ESP_LOGI(TAG, "Sensor Interface Initialized successfully");
+       }
 
-     void TDR_Soil_Sensor::dumps_config(){
+       
 
-     }
+     };
 
-     void TDR_Soil_Sensor::update_interval(){
-        
-     }
+     void TDR_Soil_Sensor::dump_config(){
+       ESP_LOGCONFIG(TAG, "TDR Soil Sensor Configurations");
+       ESP_LOGCONFIG(TAG, "TDR TX Pin : %d ",this->pin_tx);
+       ESP_LOGCONFIG(TAG, "TDR RX Pin : %d ",this->pin_rx);
+       ESP_LOGCONFIG(TAG, "TDR RE Pin : %d ",this->pin_re);
+       ESP_LOGCONFIG(TAG, "TDR DE Pin : %d ",this->pin_de);
+     };
+  
+     void TDR_Soil_Sensor::update(){
+         ESP_LOGI(TAG, "Starting measurement");
+     };
   };
 };
--- components/tdr_soil_sensor/tdr_soil_sensor.h
+++ components/tdr_soil_sensor/tdr_soil_sensor.h
@@ -4,7 +4,6 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/switch/switch.h"
-#include "esphome/components/ads1115_adc/ads1115_adc.h"
 #include<Arduino.h>
 
 namespace esphome {
@@ -14,17 +13,26 @@ namespace esphome {
        sensor::Sensor temperature_sensor_ ;
        sensor::Sensor humidity_sensor_ ;
        sensor::Sensor ec_sensor_ ;
-
+       uint8_t pin_tx;
+       uint8_t pin_rx ;
+       uint8_t pin_de ;
+       uint8_t pin_re ;
        public:
-       void setup(void) override;
-       void dumps_config(void) override;
-       void update_interval() override ;
+       void setup() override;
+       void update() override ;
+       void dump_config() override;
+       float get_setup_priority() const override { return esphome::setup_priority::HARDWARE; }
        void set_temperature_sensor(sensor::Sensor s){this->temperature_sensor_ = s ;}
        void set_humidity_sensor(sensor::Sensor s ){this->humidity_sensor_ = s ;}
        void set_ec_sensor(sensor::Sensor s) {this->ec_sensor_ = s ;}
+
+       void set_pin_tx(uint8_t pin ){this->pin_tx = pin ;}
+       void set_pin_rx(uint8_t pin ){this->pin_rx = pin ;}
+       void set_pin_de(uint8_t pin ){this->pin_de = pin ;}
+       void set_pin_re(uint8_t pin ){this->pin_re = pin ;}
     
 
-     }
+     };
   };
 };
 

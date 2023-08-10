9951ea2785c92e13e8553678f63cbf2c36d9a4a4
infotronic218 <infotronic218@gmail.com>
2023-08-11 01:00:48 +0200

    Update the source code

--- components/tdr_soil_sensor3/tdr_soil_sensor.cpp
+++ components/tdr_soil_sensor3/tdr_soil_sensor.cpp
@@ -6,6 +6,12 @@ static const char* TAG_EC = "tdr_ec_sensor ";
 static const char* TAG_WC = "tdr_wc_sensor ";
 
 
+const float TEMP_CORRECTION = 0.5;
+const float HUM_CORRECTION = 0.8;
+const float EC_SLOPE = 1.93;
+const float EC_INTERCEPT = -270.8;
+const float EC_TEMP_COEFF = 0.019;
+
 namespace esphome {
     namespace tdr_soil_sensor__{
            struct SensorData sensor_data ={0.0};
@@ -13,25 +19,41 @@ namespace esphome {
                 ESP_LOGI(TAG, "Initialization");
             }
             void TDR_Soil_Sensor::update() {
-                 ESP_LOGI(TAG, "Computing sensor data");
-                 float soil_ec = sensor_data.soil_ec*100;
-                 soil_ec = 1.93 * soil_ec - 270.8;
-                 float soil_temp = sensor_data.soil_temp ;
-                 float soil_hum = sensor_data.soil_hum ;
-                 soil_ec = soil_ec / (1.0 + 0.019 * (soil_temp -25 )) ;
-                 float soil_apparent_dieletric_constant = 1.3088 + 0.1439 * soil_hum + 0.0076 * soil_hum * soil_hum ;
-                 float soil_bulk_permittivity = soil_apparent_dieletric_constant ;
-                 float soil_pore_permittivity = 80.3 - 0.37 * (soil_temp - 20) ;
-                 float pw_ec ;
-                if (soil_bulk_permittivity > 4.1){
-                   pw_ec = (soil_pore_permittivity * soil_ec) / (soil_bulk_permittivity - 4.1) / 1000;
-                }else{
-                   pw_ec = 0 ;
-                }
+                ESP_LOGI(TAG, "Computing sensor data");
+                
+
+                float soil_hum = 0.1 * int(sensor_data.soil_hum);
+                float soil_temp = 0.1 * int(sensor_data.soil_temp);
+                int soil_ec = int(sensor_data.soil_ec);
+
+                // EC, Humidity, and Temperature Correction
+                // Soil EC Equations obtained from calibration using distilled water and a 1.1178 mS/cm solution
+                soil_ec = EC_SLOPE * soil_ec + EC_INTERCEPT;
+                soil_ec = soil_ec / (1.0 + EC_TEMP_COEFF * (soil_temp - 25));
+
+                // Study by Shang et al. 2020 showed that the temperature readings of THC-S Sensor were consistently lower than those of Teros 11 by 0.5 degrees Celsius
+                soil_temp = soil_temp + TEMP_CORRECTION;
+                
+                // 80% soil humidity correction for rockwool
+                soil_hum = soil_hum * HUM_CORRECTION;
+
+                // Updated Formula for calculating the apparent dielectric constant of soil based on Briciu-Burghina 2022. Corrected for Rockwool
+                float soil_apparent_dieletric_constant = 1.68 + 0.267 * soil_hum - 0.00476 * soil_temp + 0.000101 * soil_temp * soil_temp;
+                float soil_bulk_permittivity = soil_apparent_dieletric_constant;
+                float soil_pore_permittivity = 80.3 / (1.0 + 0.06 * (soil_temp - 25.0));
+
+                // Calculate soil pore water EC using the Briciu-Burghina 2022 model
+                float soil_pore_water_ec;
+                if (soil_bulk_permittivity > 5.5)
+                soil_pore_water_ec = (soil_ec * (soil_pore_permittivity - 5.5)) / (soil_bulk_permittivity - 5.5) / 1000.0;
+                else
+                soil_pore_water_ec = 0.0;
+
+
 
                 soil_temperature_sensor_->publish_state(soil_temp);
                 soil_humidity_sensor_->publish_state(soil_hum);
-                soil_pw_ec_sensor_->publish_state(pw_ec);
+                soil_pw_ec_sensor_->publish_state(soil_pore_water_ec);
            
             }
             void TDR_Soil_Sensor::dump_config(){
@@ -53,9 +75,10 @@ namespace esphome {
                 result = val.value();
                 }
             }
-
-            result = result/10.0 ;
+            
             sensor_data.soil_temp = result ;
+            result = result/10.0 ;
+           
             ESP_LOGD(TAG_TEMP, "Sensor new state: %.02f", result);
             // this->sensor_->raw_state = result;
             this->publish_state(result);
@@ -79,9 +102,10 @@ namespace esphome {
                 }
             }
             
-            
-            result = result/100.0 ;
             sensor_data.soil_ec = result ;
+
+            result = result/100.0 ;
+           
             ESP_LOGD(TAG_EC, "Sensor new state: %.02f", result);
             // this->sensor_->raw_state = result;
             this->publish_state(result);
@@ -104,8 +128,10 @@ namespace esphome {
                 result = val.value();
                 }
             }
-            result = result/10.0 ;
+
             sensor_data.soil_hum = result ;
+            result = result/10.0 ;
+           
             ESP_LOGD(TAG_WC, "Sensor new state: %.02f", result);
             // this->sensor_->raw_state = result;
             this->publish_state(result);
